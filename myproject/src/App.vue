4cbd57344e0e82ce216c0b00090794d5738872ed
pochiman <pochiman@yahoo.com>
2020-02-12 13:04:26 -0500

    lesson 18

---
+++ myproject/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+		<h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'My first Vue app, wooo :)'
+    }
+  }
+}
+</script>
+
+<style>
+h1{
+	color: #444;
+	font-weight: normal;
+	text-align: center;
+}
+</style>
