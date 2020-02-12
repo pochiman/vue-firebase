96eb7f23efa5951a69c4ee200d33b77924a75551
pochiman <pochiman@yahoo.com>
2020-02-12 17:32:22 -0500

    lesson 19

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
 		<h1>{{ title }}</h1>
+		<Navbar />
   </div>
 </template>
 
 <script>
+import Navbar from './Navbar'
+
 export default {
-  name: 'app',
+	name: 'app',
+	components: {
+		Navbar
+	},
   data () {
     return {
       title: 'My first Vue app, wooo :)'
