42aa4007d9a56ef9bb3ef8d51f8ca0a9bd17e7bd
pochiman <pochiman@yahoo.com>
2020-02-12 21:04:37 -0500

    lesson 21

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -2,27 +2,32 @@
   <div id="app">
 		<h1>{{ title }}</h1>
 		<Navbar />
-		<nav>
-			<ul>
-				<li><a href="">Home</a></li>
-				<li><a href="">About</a></li>
-				<li><a href="">Contact</a></li>
-			</ul>
-		</nav>
+		<AllFriends :friends="friends" />
+		<OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
 import Navbar from './Navbar'
+import AllFriends from './AllFriends'
+import OnlineFriends from './OnlineFriends'
 
 export default {
 	name: 'app',
 	components: {
-		Navbar
+		Navbar,
+		OnlineFriends,
+		AllFriends
 	},
   data () {
     return {
-      title: 'My first Vue app, wooo :)'
+			title: 'My first Vue app, wooo :)',
+			friends: [
+				{ name: 'Mario', online: true },
+				{ name: 'Luigi', online: false },
+				{ name: 'Toad', online: true },
+				{ name: 'Bowser', online: false }
+			]
     }
   }
 }
