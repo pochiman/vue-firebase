1d33348e30ef4629d11aa8321d1d672162808a6c
pochiman <pochiman@yahoo.com>
2020-03-12 15:04:09 -0400

    lesson 81

--- geo-ninjas/src/components/auth/Signup.vue
+++ geo-ninjas/src/components/auth/Signup.vue
@@ -14,6 +14,7 @@
 				<label for="alias">Alias:</label>
 				<input type="text" name="alias" v-model="alias">
 			</div>
+			<p class="red-text center" v-if="feedback">{{ feedback }}</p>
 			<div class="field center">
 				<button class="btn deep-purple">Signup</button>
 			</div>
@@ -22,18 +23,40 @@
 </template>
 
 <script>
+import slugify from 'slugify'
+import db from '@/firebase/init'
+
 export default {
 	name: 'Signup',
 	data(){
 		return {
 			email: null,
 			password: null,
-			alias: null
+			alias: null,
+			feedback: null,
+			slug: null
 		}
 	},
 	methods: {
 		signup(){
-			
+			if(this.alias){
+				this.slug = slugify(this.alias, {
+					replacement: '-',
+					remove: /[$*_+~.()'"!\-:@]/g,
+					lower: true
+				})
+				let ref = db.collection('users').doc(this.slug)
+				ref.get().then(doc => {
+					if(doc.exists){
+						this.feedback = 'This alias already exists'
+					}	else {
+						this.feedback = 'This alias is free to use'
+					}
+				})
+				console.log(this.slug)
+			}	else {
+				this.feedback = "You must enter an alias"
+			}
 		}
 	}
 }
