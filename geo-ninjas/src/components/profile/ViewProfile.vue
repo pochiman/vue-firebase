1b1624afcff3eeca13d0c0e3b03c25035b7b971d
pochiman <pochiman@yahoo.com>
2020-03-13 18:20:24 -0400

    lesson 97

--- geo-ninjas/src/components/profile/ViewProfile.vue
+++ geo-ninjas/src/components/profile/ViewProfile.vue
@@ -72,7 +72,7 @@ export default {
 				this.feedback = null
 				db.collection('comments').add({
 					to: this.$route.params.id,
-					from: this.user.id,
+					from: this.user.alias,
 					content: this.newComment,
 					time: Date.now()
 				}).then(() => {
@@ -85,3 +85,18 @@ export default {
 	}
 }
 </script>
+
+<style>
+.view-profile .card{
+	padding: 20px;
+	margin-top: 60px;
+}
+.view-profile h2{
+	font-size: 2em;
+	margin-top: 0;
+}
+.view-profile li{
+	padding: 10px;
+	border-bottom: 1px solid #eee;
+}
+</style>
